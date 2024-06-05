fea157de3513704980df2b7020c50b434b9d4255
Daniel Zhang <xiaozhangshiw@163.com>
2024-06-05 23:11:05 +0800

    修正权限问题

--- project/module-ingame/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/ingame/command/AiyatsbusMenuCommand.kt
+++ project/module-ingame/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/ingame/command/AiyatsbusMenuCommand.kt
@@ -4,12 +4,12 @@ import com.mcstarrysky.aiyatsbus.module.ingame.ui.MainMenuUI
 import org.bukkit.entity.Player
 import taboolib.common.platform.command.CommandBody
 import taboolib.common.platform.command.CommandHeader
+import taboolib.common.platform.command.PermissionDefault
 import taboolib.common.platform.command.mainCommand
 
 @CommandHeader("enchants")
 object AiyatsbusMenuCommand {
-
-    @CommandBody
+    @CommandBody(permissionDefault = PermissionDefault.TRUE)
     val sub = mainCommand {
         execute<Player> { sender, _, _ -> MainMenuUI.open(sender) }
     }
