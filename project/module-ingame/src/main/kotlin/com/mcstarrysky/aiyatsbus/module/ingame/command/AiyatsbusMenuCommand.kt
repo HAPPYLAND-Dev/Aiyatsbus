5fc112b3e9213230d19ad94acac43b1192039933
Daniel Zhang <xiaozhangshiw@163.com>
2024-06-05 22:29:46 +0800

    调整命令

--- project/module-ingame/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/ingame/command/AiyatsbusMenuCommand.kt
+++ project/module-ingame/src/main/kotlin/com/mcstarrysky/aiyatsbus/module/ingame/command/AiyatsbusMenuCommand.kt
@@ -1,12 +1,16 @@
 package com.mcstarrysky.aiyatsbus.module.ingame.command
 
-import com.mcstarrysky.aiyatsbus.module.ingame.command.subcommand.menuSubCommand
+import com.mcstarrysky.aiyatsbus.module.ingame.ui.MainMenuUI
+import org.bukkit.entity.Player
 import taboolib.common.platform.command.CommandBody
 import taboolib.common.platform.command.CommandHeader
+import taboolib.common.platform.command.mainCommand
 
 @CommandHeader("enchants")
 object AiyatsbusMenuCommand {
 
     @CommandBody
-    val sub = menuSubCommand
+    val sub = mainCommand {
+        execute<Player> { sender, _, _ -> MainMenuUI.open(sender) }
+    }
 }
